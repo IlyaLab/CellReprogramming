00a3c691559c6a37692b99a3784c1081122008d0
Bahar Tercan <62409751+bhrtrcn@users.noreply.github.com>
2022-03-22 12:38:59 -0700

    Construct BNs

---
+++ ProB_Monocyte_Example/run_bn_construction_prob_mono.sh
@@ -0,0 +1,18 @@
+Attractors_folder="Sampled_Network_Attractors"
+output_folder="RESULTS"
+mkdir $output_folder
+num_bns=500
+n_sets=500
+
+for j in $(seq 1 1 $n_sets); do
+	for i in $(seq 1 1 $num_bns); do
+		mkdir -p ${output_folder}/$j/$i
+	done
+done
+
+for j in $(seq 1 1 $n_sets); do
+	for i in $(seq 1 1 $num_bns); do
+
+	./../BN_Construction/program --att_info_file ${Attractors_folder}/$j/attractor_info.txt --attractors_file ${Attractors_folder}/$j/attractors$i.txt --output_directory ${output_folder}/$j/$i --num_genes 10
+	done
+done
