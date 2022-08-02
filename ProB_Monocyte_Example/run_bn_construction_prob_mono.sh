f87e074dc915e1216a48ef91deba7925d185e838
Bahar Tercan <62409751+bhrtrcn@users.noreply.github.com>
2022-08-02 11:09:37 -0700

    Update run_bn_construction_prob_mono.sh

--- ProB_Monocyte_Example/run_bn_construction_prob_mono.sh
+++ ProB_Monocyte_Example/run_bn_construction_prob_mono.sh
@@ -7,6 +7,6 @@ n_sets=500
 for j in $(seq 1 1 $n_sets); do
 	for i in $(seq 1 1 $num_bns); do
         mkdir -p ${output_folder}/$j/$i
-	   ./BN_Construction/program --att_info_file ${Attractors_folder}/$j/attractor_info.txt --attractors_file ${Attractors_folder}/$j/attractors$i.txt --output_directory ${output_folder}/$j/$i --num_genes 10
+	   ./../BN_Construction/program --att_info_file ${Attractors_folder}/$j/attractor_info.txt --attractors_file ${Attractors_folder}/$j/attractors$i.txt --output_directory ${output_folder}/$j/$i --num_genes 10
 	done
 done
