b2f52b40ba506daf836a0d29a1c8bd930030eb91
Bahar Tercan <62409751+bhrtrcn@users.noreply.github.com>
2022-09-14 11:31:17 -0700

    Delete FindAttractors_Interventions.sh

--- ProB_Monocyte_Example/FindAttractors_Interventions.sh
+++
@@ -1,8 +0,0 @@
-n_sets=500
-attractor_file="Attractors.csv"
-result_folder="RESULTS"
-output_folder="Output"
-attractor_folder="Sampled_Network_Attractors"
-for j in $(seq 1 1 $n_sets); do
-	julia  FindAttractors_and_Intervention.jl $j ${attractor_file}  ${attractor_folder}/$j/ind_matrix.txt ${result_folder}/$j  ${output_folder}/$j
-done
